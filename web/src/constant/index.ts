1ee7b6a006646900e633c8a194e4ac02f4db985a
jesoria <jesoria@indra.es>
2025-07-21 14:50:22 +0200

    feat: first commit

---
+++ web/src/constant/index.ts
@@ -0,0 +1,28 @@
+// export const DEFAULT_TRACE_LOG_FILE = `${process.env.PWD}/log/trace.log`;
+
+/** Environment */
+export const ENV_PROTOCOL = 'PROTOCOL';
+export const ENV_IP = 'IP';
+export const ENV_PORT = 'PORT';
+export const ENV_API_ROOT = 'API_ROOT';
+
+export const ENV_ACCESS_LOG_FILE = 'ACCESS_LOG_FILE';
+export const ENV_ACCESS_LOG_FILE_TIME_INTERVAL = 'ACCESS_LOG_FILE_TIME_INTERVAL';
+export const ENV_ACCESS_LOG_FILE_SIZE_INTERVAL = 'ACCESS_LOG_FILE_SIZE_INTERVAL';
+
+/** Argv */
+export const ARGV_PROTOCOL = '--protocol';
+export const ARGV_IP = '--ip';
+export const ARGV_PORT = '--port';
+export const ARGV_API_ROOT = '--api-root';
+
+export const ARGV_ACCESS_LOG_FILE = '--access-log-file';
+export const ARGV_ACCESS_LOG_FILE_TIME_INTERVAL = '--access-log-file-time-interval';
+export const ARGV_ACCESS_LOG_FILE_SIZE_INTERVAL = '--access-log-file-size-interval';
+
+/** Decorators */
+export const DECORATOR_GET_NAME = '@Get';
+export const DECORATOR_POST_NAME = '@Post';
+export const DECORATOR_PUT_NAME = '@Put';
+export const DECORATOR_DELETE_NAME = '@Delete';
+export const DECORATOR_CONTROLLER_NAME = '@Controller';
